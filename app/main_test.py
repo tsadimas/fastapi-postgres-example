c34704f4e622b3673dd5c8e5a145c62ed7d6212d
Anargyros Tsadimas <argiris@norbloc.com>
2021-05-12 10:58:56 +0300

    added requirements for testing and another test

--- app/main_test.py
+++ app/main_test.py
@@ -9,3 +9,17 @@ def test_read_main():
     response = client.get("/items")
     assert response.status_code == 200
 
+def test_create_user():
+    response = client.post(
+        "/users/",
+        headers={'Content-Type': 'application/json'},
+        json={"email": "user@test.com", "password": "pass123"},
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+    "email": "user@test.com",
+    "id": 1,
+    "is_active": True,
+    "items": []
+    }
+
